a3a7bfcf6c950961e74674dd2a3ad67836facf31
Alex Ushkov <alexushkov82@gmail.com>
2022-04-04 10:22:19 +0300

    simple PageView

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news/main.dart';
 import 'data.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer' as devtools show log;
@@ -13,42 +14,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('News'),
       ),
-      body: SafeArea(
-        child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: Item.samples.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Scaffold(
-                        appBar: AppBar(
-                          title: Text(Item.samples[index].name),
-                        ),
-                        body: Container(),
-                      );
-                    }
-                  )
-                );
-              },
-              child: buildItemCard(Item.samples[index]),
-            );
-          },
-        ),
+      body: PageView(
+        controller: _controller,
+        children: [
+          Page1(),
+          Page2(),
+          Page3(),
+        ],
       ),
     );
   }
 
-  Widget buildItemCard(Item item) {
+  Widget Page1() {
     return Card(
       elevation: 2,
       shape: RoundedRectangleBorder(
@@ -59,7 +52,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: <Widget>[
             Text(
-              item.name,
+              'One',
               style: TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.w500,
@@ -69,7 +62,8 @@ class _HomePageState extends State<HomePage> {
               height: 15,
             ),
             Text(
-              item.info,
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
+                  ' do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.w400,
@@ -80,5 +74,76 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget Page2() {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Two',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
+                  ' do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Page3() {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Three',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
+                  ' do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
 
 }
