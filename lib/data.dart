847665d6a06465b830133380469b102ce618244a
Alex Ushkov <alexushkov82@gmail.com>
2022-04-07 11:36:10 +0300

    problems w decoding

--- lib/data.dart
+++ lib/data.dart
@@ -1,41 +1,54 @@
+import 'dart:convert';
+import 'package:xml/xml.dart';
+import 'package:html_unescape/html_unescape.dart';
+import 'package:xml/xml.dart' as xml;
+
 class Item {
   String title;
   String link;
-  String description;
+  List<String> description;
   String date;
   String guid;
 
-  Item(
-      this.title,
-      this.link,
-      this.description,
-      this.date,
-      this.guid,
-      );
+  Item({
+    required this.title,
+    required this.link,
+    required this.description,
+    required this.date,
+    required this.guid,
+  });
 
-  // static List<Item> samples = [
-  //   Item(
-  //     'Альфа-Банк автоматизировал работу МФО с участниками закупок',
-  //     'https://alfabank.ru/press/news/2021/7/13/86418.html?from=alfa_rss_news',
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-  //     'Tue, 13 Jul 2021 10:16:00 GMT',
-  //     'https://alfabank.ru/press/news/2021/7/13/86418.html',
-  //   ),
-  //   Item(
-  //     'Альфа-Банк возглавил рэнкинг организаторов дебютных выпусков корпоративных облигаций от Cbonds',
-  //     'https://alfabank.ru/press/news/2021/7/12/86417.html?from=alfa_rss_news',
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-  //     'Mon, 12 Jul 2021 16:13:00 GMT',
-  //     'https://alfabank.ru/press/news/2021/7/12/86417.html',
-  //   ),
-  //   Item(
-  //     'Альфа-Банк запустил сервис приема платежей по QR-коду для оффлайн-магазинов',
-  //     'https://alfabank.ru/press/news/2021/7/9/86293.html?from=alfa_rss_news',
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-  //     'Fri, 09 Jul 2021 09:03:00 GMT',
-  //     'https://alfabank.ru/press/news/2021/7/9/86293.html',
-  //   ),
-  //
-  // ];
+  @override
+  String toString() {
+    return title + '\n' + link + '\n' + description.toString()  + '\n' + date + '\n' + guid;
+  }
+
+  factory Item.fromElement(XmlElement element) {
+    List<String> description = [element.findElements('description').single.text];
+    return Item(
+      title: /*unhtml.convert(element
+          .findElements('title')
+          .single
+          .text),*/
+      element
+          .findElements('title')
+          .single
+          .text,
+      link: element
+        .findElements('link')
+        .single
+        .text,
+      description: description,
+      date: element
+          .findElements('pubDate')
+          .single
+          .text,
+      guid: element
+          .findElements('guid')
+          .single
+          .text,
+    );
+
+  }
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:xml/xml.dart' as xml;
+import 'package:http/http.dart' as http;
 import 'package:xml/xml_events.dart';
+import 'package:xml2json/xml2json.dart';
 import 'data.dart';
 import 'dart:io';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:developer' as devtools show log;
+import 'dart:async';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,35 +26,51 @@ class _HomePageState extends State<HomePage> {
     initialPage: 0,
   );
 
-
   Future<List<Item>> getItemsFromXML(BuildContext context) async {
-    // final url = Uri.parse('https://alfabank.ru/_/rss/_rss.html?subtype=1&category=2&city=21');
-    // final request = await HttpClient().getUrl(url);
-    // final HttpClientResponse response = await request.close();
-    // final stringData = await response.transform(utf8.decoder).join();
-    // print(stringData);
-    // await response
-    //     .transform(utf8.decoder)
-        // .toXmlEvents()
-        // .normalizeEvents()
-        // .forEachEvent(onText: (event) => print(event.text));
+    try {
+      final url = Uri.parse('https://alfabank.ru/_/rss/_rss.html?subtype=1&category=2&city=21');
+      Map<String, String> headers = {"Content-Type": "text/html,application/xml; charset=utf-8"};
+      final response = await http.get(url, headers: headers);
+      if (response.statusCode == 200) {
+        print(response.bodyBytes);
+        final document = xml.XmlDocument.parse(utf8.decode(response.bodyBytes, allowMalformed: true));
+        var xmlParsedFile = document
+            .findAllElements('item')
+            .map<Item>((e) => Item.fromElement(e))
+            .toList();
+        return xmlParsedFile;
+      } else {
+        return throw Exception('Failed to load');
+      }
+    } catch(e) {
+      print(e);
+      return [];
+    }
 
-    // return [];
 
+    /*
+    String file = await DefaultAssetBundle.of(context).loadString('assets/data/data.xml');
+    var document = xml.XmlDocument.parse(file);
+    var xmlParsedFile = document
+        .findAllElements('item')
+        .map<Item>((e) => Item.fromElement(e))
+        .toList();
+    return xmlParsedFile;
+    */
+  }
 
+  @override
+  void initState() {
+    Timer.periodic(const Duration(minutes: 5), (Timer timer) {
+      setState(() {});
+    });
+    super.initState();
 
-    String xmlString = await DefaultAssetBundle.of(context).loadString('assets/data/data.xml');
-    var raw = xml.parse(xmlString);
-    var elements = raw.findAllElements('item');
-    return elements.map((element){
-      return Item(
-        element.findElements('title').first.text,
-        element.findElements('link').first.text,
-        element.findElements('description').first.text,
-        element.findElements('pubDate').first.text,
-        element.findElements('guid').first.text,
-      );
-    }).toList();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -59,9 +81,10 @@ class _HomePageState extends State<HomePage> {
       ),
       body: FutureBuilder(
         future: getItemsFromXML(context),
-        builder: (context, data) {
+        builder: (BuildContext context, AsyncSnapshot data) {
           if (data.hasData) {
             List<Item> items = data.data as List<Item>;
+            FlutterNativeSplash.remove();
             return PageView.builder(
               physics: BouncingScrollPhysics(),
               itemCount: items.length,
@@ -80,18 +103,24 @@ class _HomePageState extends State<HomePage> {
                               initialUrl: items[index].link,
                             ),
                           );
-                          },
+                        },
                       ),
                     );
-                    },
+                  },
                   child: itemCard(items[index]),
                 );
-                },
+              },
             );
           } else {
             return Center(child: CircularProgressIndicator());
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
@@ -106,6 +135,7 @@ class _HomePageState extends State<HomePage> {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
           child: Column(
             children: <Widget>[
               Text(
@@ -119,7 +149,7 @@ class _HomePageState extends State<HomePage> {
                 height: 15,
               ),
               Text(
-                item.description,
+                item.description[0],
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w400,
